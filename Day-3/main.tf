27a4a1adbfbd20c12642b0379663571de38cf29d
Dhareppa2023 <136324648+Dhareppa2023@users.noreply.github.com>
2023-12-18 15:36:56 +0530

    changed

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "" # replace this
   instance_type_value = "t2.micro"
   
 }
